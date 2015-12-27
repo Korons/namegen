1f89cbc0a2a6539df7ec032ce9885c9bf15a5392
Korons <william.sweeting@vfemail.net>
2015-12-27 04:04:39 -0500

    Added date of birth and phone number

--- namegen.py
+++ namegen.py
@@ -10,5 +10,20 @@ def generate_name():
 
     print first, last
 
-generate_name()
+def generate_phonenum():
+	""" Generate a phone number """
+	phone_first = random.randint(111,999)
+	phone_second = random.randint(111,999)
+	phone_third = random.randint(1111,9999)
+	print  phone_first, phone_second, phone_third
 
+def generate_dob():
+	""" Generate date of birth """
+	year = random.randint(1950,2000)
+	month = random.randint(1,12)
+	day = random.randint(1,30)
+	print year,'/',month,'/',day
+
+generate_name()
+generate_phonenum()
+generate_dob()
