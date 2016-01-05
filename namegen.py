f4c6c34355f8dc4dc8ea55886bb1b949c87af1d0
Korons <william.sweeting@vfemail.net>
2016-01-05 17:19:06 -0500

    Added credit card gen

--- namegen.py
+++ namegen.py
@@ -3,6 +3,141 @@ import random
 import argparse
 import sys
 import string
+import copy
+from random import Random
+
+# Creditcard code from https://github.com/grahamking/darkcoding-credit-card
+visaPrefixList = [
+        ['4', '5', '3', '9'],
+        ['4', '5', '5', '6'],
+        ['4', '9', '1', '6'],
+        ['4', '5', '3', '2'],
+        ['4', '9', '2', '9'],
+        ['4', '0', '2', '4', '0', '0', '7', '1'],
+        ['4', '4', '8', '6'],
+        ['4', '7', '1', '6'],
+        ['4']]
+
+mastercardPrefixList = [
+        ['5', '1'], ['5', '2'], ['5', '3'], ['5', '4'], ['5', '5']]
+
+amexPrefixList = [['3', '4'], ['3', '7']]
+
+discoverPrefixList = [['6', '0', '1', '1']]
+
+dinersPrefixList = [
+        ['3', '0', '0'],
+        ['3', '0', '1'],
+        ['3', '0', '2'],
+        ['3', '0', '3'],
+        ['3', '6'],
+        ['3', '8']]
+
+enRoutePrefixList = [['2', '0', '1', '4'], ['2', '1', '4', '9']]
+
+jcbPrefixList = [['3', '5']]
+
+voyagerPrefixList = [['8', '6', '9', '9']]
+
+
+def completed_number(prefix, length):
+    """
+    'prefix' is the start of the CC number as a string, any number of digits.
+    'length' is the length of the CC number to generate. Typically 13 or 16
+    """
+
+    ccnumber = prefix
+
+    # generate digits
+
+    while len(ccnumber) < (length - 1):
+        digit = str(generator.choice(range(0, 10)))
+        ccnumber.append(digit)
+
+    # Calculate sum
+
+    sum = 0
+    pos = 0
+
+    reversedCCnumber = []
+    reversedCCnumber.extend(ccnumber)
+    reversedCCnumber.reverse()
+
+    while pos < length - 1:
+
+        odd = int(reversedCCnumber[pos]) * 2
+        if odd > 9:
+            odd -= 9
+
+        sum += odd
+
+        if pos != (length - 2):
+
+            sum += int(reversedCCnumber[pos + 1])
+
+        pos += 2
+
+    # Calculate check digit
+
+    checkdigit = ((sum / 10 + 1) * 10 - sum) % 10
+
+    ccnumber.append(str(checkdigit))
+
+    return ''.join(ccnumber)
+
+
+def credit_card_number(rnd, prefixList, length, howMany):
+
+    result = []
+
+    while len(result) < howMany:
+
+        ccnumber = copy.copy(rnd.choice(prefixList))
+        result.append(completed_number(ccnumber, length))
+
+    return result
+
+
+def output(title, numbers):
+
+    result = []
+    result.append(title)
+    result.append('-' * len(title))
+    result.append('\n'.join(numbers))
+    result.append('')
+
+    return '\n'.join(result)
+
+#
+# Main
+#
+
+generator = Random()
+generator.seed()        # Seed from current time
+
+mastercard = credit_card_number(generator, mastercardPrefixList, 16, 1)
+visa16 = credit_card_number(generator, visaPrefixList, 16, 1)
+visa13 = credit_card_number(generator, visaPrefixList, 13, 1)
+amex = credit_card_number(generator, amexPrefixList, 15, 1)
+# Minor cards
+
+discover = credit_card_number(generator, discoverPrefixList, 16, 1)
+diners = credit_card_number(generator, dinersPrefixList, 14, 1)
+enRoute = credit_card_number(generator, enRoutePrefixList, 15, 1)
+jcb = credit_card_number(generator, jcbPrefixList, 16, 1)
+voyager = credit_card_number(generator, voyagerPrefixList, 15, 1)
+mastercard =''.join(mastercard)
+visa16 = ''.join(visa16)
+visa13 = ''.join(visa13)
+amex = ''.join(amex)
+discover = ''.join(discover)
+diners = ''.join(diners)
+enRoute = ''.join(enRoute)
+jcb = ''.join(jcb)
+voyager = ''.join(voyager)
+
+
+
 
 parser = argparse.ArgumentParser(description='Generate fake data')
 parser.add_argument("-n", help="Use female names", choices=['male','female'])
@@ -10,6 +145,7 @@ parser.add_argument("-o", help="Output to csv file")
 parser.add_argument("-c", help="Country")
 parser.add_argument("-y", help="Year range Ex 1950-1970")
 parser.add_argument("-l", help="The number of times to loop")
+parser.add_argument("-k", help="Creditcard type", choices=['mastercard','visa16','visa13','amex','discover','diners','enRoute','jcb','voyager'], default='mastercard')
 args = parser.parse_args()
 
 #Gobal vars
@@ -197,7 +333,27 @@ def generate_country():
 			f.close
 		else:
 			print country
-
+def creditcard():
+	if args.k == 'mastercard':
+		print mastercard[0]
+	elif args.k == 'visa16':
+		print visa16
+	elif args.k == 'visa13':
+		print visa13
+	elif args.k == 'amex':
+		print amex
+	elif args.k == 'discover':
+		print discover
+	elif args.k == 'diners':
+		print diners
+	elif args.k == 'enRoute':
+		print enRoute
+	elif args.k == 'jcb':
+		print jcb
+	elif args.k == 'voyager':
+		print voyager
+	else:
+		print "Error in credit card gen\nExiting"
 # This generates the phone number
 def generate_phonenum():
 	alab_codes_usa = [205, 251, 256, 334, 938]
@@ -412,7 +568,7 @@ generate_name()
 generate_dob()
 generate_country()
 generate_phonenum()
-
+creditcard()
 if args.l:
 	run_range = args.l
 	# we start at 1 and 0 here because we already ran once above this
